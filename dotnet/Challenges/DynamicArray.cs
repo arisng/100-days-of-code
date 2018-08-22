34f15f1f36bb841420eb7588c47d9b3463234808
Anh Nguyen <nda1291@outlook.com>
2018-08-23 00:17:52 +0700

    R1D1 - dynamic array challenge

---
+++ dotnet/Challenges/DynamicArray.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace dotnet.Challenges
+{
+    class DynamicArraySolution
+    {
+        // Complete the dynamicArray function below.
+        static List<int> DynamicArray(int n, List<List<int>> queries)
+        {
+            var result = new List<int>();
+            var seqList = new List<List<int>>(n);
+            var lastAns = 0;
+
+            for (var i = 0; i < queries.Count; i++)
+            {
+                var query = queries[i];
+                var x = query[1];
+                var y = query[2];
+                var seqIndex = (x ^ lastAns) % n;
+
+                if (query[0] == 1) 
+                {
+                    // query1
+                    if (seqList.Count < n)
+                    {
+                        seqList.Add(new List<int>());
+                    }
+                    seqList[seqIndex].Add(y);
+                }
+                else 
+                {
+                    // query2
+                    var index = y % seqList[seqIndex].Count;
+                    lastAns = seqList[seqIndex][index];
+                    result.Add(lastAns);
+                }
+            }
+            
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var list = new List<int>();
+
+                using (var file = new StreamReader(@"F:\My Work Space\Github\100-days-of-code\dotnet\TestCases\DynamicArray\DynamicArray2.txt"))
+                {
+                    string[] nq = file.ReadLine().TrimEnd().Split(' ');
+
+                    int n = Convert.ToInt32(nq[0]);
+
+                    int q = Convert.ToInt32(nq[1]);
+
+                    List<List<int>> queries = new List<List<int>>();
+
+                    for (int i = 0; i < q; i++)
+                    {
+                        queries.Add(file.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+                    }
+
+                    List<int> result = DynamicArray(n, queries);
+                    Console.WriteLine(String.Join("\n", result));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
--- r1-log.md
+++ r1-log.md
@@ -1,13 +1,15 @@
-# #100DaysOfCode Log - Round 1 - [Your Name Here]
+# #100DaysOfCode Log - Round 1 - [Aris Nguyen]
 
 The log of my #100DaysOfCode challenge. Started on [August 22, 2018].
 
 ## Log
 
 ### R1D1 
-- Setup Visual Studio Code in AWS
-- clone 100DaysOfCode repo from github into AWS
+- Setup Visual Studio Code in my workspace
+- At this moment, my workspace consists of the AWS and my personal laptop
+- Clone 100DaysOfCode repo from github into my workspace
 - From now on, for each problem in hackerank, using this repo to keep track of the code
+- Learn how to run and debug c# code in VSC (Visual Studio Code)
 - github repo: https://github.com/arisng/100-days-of-code
 
 ### R1D2
